ed65308fcd88094939f684e1302ef9d5f192cc90
yzamarie <yzamarie423@gmail.com>
2024-05-17 12:45:16 +0800

    FunctionFrontend.sol

--- FunctionFrontend.sol
+++ FunctionFrontend.sol
@@ -1,34 +1,17 @@
-// Import ethers.js library
-const { ethers } = require("ethers");
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// Initialize provider (like MetaMask or Infura)
-const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+contract SimpleContract {
+    uint256 public value;
 
-// Address of the deployed contract
-const contractAddress = "0x123...";
+    event ValueChanged(uint256 newValue);
 
-// ABI (Application Binary Interface) of the contract
-const abi = [
-    "function getNumber() public view returns (uint256)",
-    "function setNumber(uint256 _number) public"
-];
+    function setValue(uint256 _newValue) public {
+        value = _newValue;
+        emit ValueChanged(_newValue);
+    }
 
-// Connect to the contract
-const contract = new ethers.Contract(contractAddress, abi, provider);
-
-// Get the current number
-async function getCurrentNumber() {
-    const currentNumber = await contract.getNumber();
-    console.log("Current number:", currentNumber.toString());
+    function getValue() public view returns (uint256) {
+        return value;
+    }
 }
-
-// Set a new number
-async function setNewNumber(newNumber) {
-    await contract.setNumber(newNumber);
-    console.log("Number set to:", newNumber);
-}
-
-// Call functions
-getCurrentNumber();
-setNewNumber(42);
-getCurrentNumber();
